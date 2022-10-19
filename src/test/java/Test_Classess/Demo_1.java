6a0bd365dd1577668369f46ff2115980b2e18b10
Amir <Amir@LAPTOP-0G9F9NDF>
2022-10-19 14:40:27 +0530

    code is ready

---
+++ src/test/java/Test_Classess/Demo_1.java
@@ -0,0 +1,12 @@
+package Test_Classess;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo_1 {
+	@Test
+	public void demo() {
+		Reporter.log("Start Git Now",true);
+	}
+
+}
